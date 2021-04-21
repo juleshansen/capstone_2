bf16bf2b0c86f46a5fd81722f8c48eedcfa51b1d
juleshansen <jules.hansen93@gmail.com>
2021-04-21 16:15:44 -0600

    made video face detector

--- src/detect_face_image.py
+++ src/detect_face_image.py
@@ -1,8 +1,9 @@
 import cv2
+import sys
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img = cv2.imread('../data/ExtendedYaleB/yaleB13/yaleB13_P00A-005E-10.pgm')
+img = cv2.imread(sys.argv[1])
 faces = face_cascade.detectMultiScale(img, 1.1, 4)
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 cv2.imshow('img', img)
-cv2.waitKey()
+cv2.waitKey(0) & 0xff
