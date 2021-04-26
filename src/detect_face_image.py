f70fa1b8eee369d2f7d7bd973c4521ca4b363122
juleshansen <jules.hansen93@gmail.com>
2021-04-25 20:18:32 -0600

    starter models

--- src/detect_face_image.py
+++ src/detect_face_image.py
@@ -1,9 +1,21 @@
 import cv2
 import sys
+import tensorflow as tf;
+import numpy as np
+
+model = tf.keras.models.load_model('../model/model_0.2');
+em_labels = np.array(['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'])
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread(sys.argv[1])
 faces = face_cascade.detectMultiScale(img, 1.1, 4)
 for (x, y, w, h) in faces:
+    face = img[y:y+h,x:x+w]
+    face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+    face = cv2.resize(face, (48, 48), interpolation=cv2.INTER_AREA)
+    face = face.reshape(1, 48, 48, 1)
+    emotion = em_labels[model.predict(face).argmax()]
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-cv2.imshow('img', img)
-cv2.waitKey(0) & 0xff
+    cv2.putText(img, emotion, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+cv2.imshow('img', img);
+cv2.waitKey(0) & 0xff;
--- src/detect_face_video.py
+++ src/detect_face_video.py
@@ -6,6 +6,7 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
+        cv2.putText(img, 'Emotion', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
